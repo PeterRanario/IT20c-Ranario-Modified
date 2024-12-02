cae95c82a50fa656ad061f6918773d2974afbdeb
PeterRanario <peterranario11@gmail.com>
2024-12-03 00:27:14 +0800

    Added Array List

--- It20c-Ranario/src/ArrayList/ItemManager.java
+++ It20c-Ranario/src/ArrayList/ItemManager.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ArrayList;
 
 import java.util.ArrayList;
@@ -35,5 +31,6 @@ public class ItemManager {
             }
         }
     }
+    
 }
 
