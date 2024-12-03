3429f6a0f5ee896c645dcd5a5d6d0f5cb2c199db
PeterRanario <peterranario11@gmail.com>
2024-12-04 00:58:39 +0800

    To print arrays

--- It20c-Ranario/src/topic_6_sorting/BubbleSort.java
+++ It20c-Ranario/src/topic_6_sorting/BubbleSort.java
@@ -25,4 +25,10 @@ public class BubbleSort {
         }
     }
 
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
