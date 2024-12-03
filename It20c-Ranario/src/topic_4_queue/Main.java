b5ba3918c4a7d95b132ce5d213ec7112e9e4e2c6
PeterRanario <peterranario11@gmail.com>
2024-12-04 00:45:18 +0800

    Added scanner inputs

--- It20c-Ranario/src/topic_4_queue/Main.java
+++ It20c-Ranario/src/topic_4_queue/Main.java
@@ -12,5 +12,20 @@ import java.util.Scanner;
  * @author Peter
  */
 public class Main {
-    
+
+    Scanner sc = new Scanner(System.in);
+    Queue customerQueue = new Queue();
+
+    System.out.println ("Hello! Good Day Ma'am/Sir!");
+             
+        while(true) {
+                               
+            System.out.println("Choose what you want:"
+                + " 1. Display Queue."
+                + " 2. Add Customer. "
+                + " 3. Serve Customer."
+                + " 4. Exit");
+
+        int choices = sc.nextInt();
+    }
 }
