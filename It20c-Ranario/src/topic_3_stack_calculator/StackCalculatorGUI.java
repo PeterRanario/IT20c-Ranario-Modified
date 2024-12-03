577cdcd974dd00a72704b65656d0eef5292be4b1
PeterRanario <peterranario11@gmail.com>
2024-12-03 23:55:40 +0800

    Initial commit on Stack calculator GUI

--- It20c-Ranario/src/topic_3_stack_calculator/StackCalculatorGUI.java
+++ It20c-Ranario/src/topic_3_stack_calculator/StackCalculatorGUI.java
@@ -5,10 +5,21 @@
  */
 package topic_3_stack_calculator;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Stack;
+
 /**
  *
  * @author Peter
  */
-public class StackCalculatorGUI {
-    
+public class StackCalculatorGUI extends JFrame implements ActionListener {
+    private Stack<Integer> numberStack = new Stack<>();
+    private JTextArea displayArea;
+
+    public StackCalculatorGUI() {
+        
+    }
 }
