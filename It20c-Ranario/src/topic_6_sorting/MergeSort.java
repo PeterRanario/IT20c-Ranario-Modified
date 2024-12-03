0d500e1db6256adbcb6dc2d3d2441c07bef1d01e
PeterRanario <peterranario11@gmail.com>
2024-12-04 01:12:29 +0800

    Modified some changes

---
+++ It20c-Ranario/src/topic_6_sorting/MergeSort.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic_6_sorting;
+
+/**
+ *
+ * @author Peter
+ */
+public class MergeSort {
+
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) {
+            return;
+        }
+    }
+
+}
