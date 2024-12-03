526ba9029a56c0227036d1a1102e78b613ea3b22
PeterRanario <peterranario11@gmail.com>
2024-12-03 08:53:15 +0800

    Added Method to add a new node

--- It20c-Ranario/src/topic_2_linkedlist_int/Linkedlist.java
+++ It20c-Ranario/src/topic_2_linkedlist_int/Linkedlist.java
@@ -10,10 +10,11 @@ package topic_2_linkedlist_int;
  * @author Peter
  */
 public class Linkedlist {
-    
+
     private Node head;
 
     private static class Node {
+
         int value;
         Node next;
 
@@ -21,5 +22,19 @@ public class Linkedlist {
             this.value = value;
             this.next = null;
         }
-    }   
+    }
+
+    // Method to add a new node at the end of the list
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
 }
