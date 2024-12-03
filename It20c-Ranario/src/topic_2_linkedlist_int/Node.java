b7204cff84020dbee9f7b9a62c059308363a9dbd
PeterRanario <peterranario11@gmail.com>
2024-12-03 08:51:44 +0800

    Added initializing variable and constructor on Node file.

--- It20c-Ranario/src/topic_2_linkedlist_int/Node.java
+++ It20c-Ranario/src/topic_2_linkedlist_int/Node.java
@@ -10,5 +10,15 @@ package topic_2_linkedlist_int;
  * @author Peter
  */
 public class Node {
-    
+
+    //Initalizing variable data and Node next
+    int data;
+    Node next;
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
 }
