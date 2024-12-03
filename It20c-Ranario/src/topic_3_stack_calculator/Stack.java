bb2151506ae80165d3cc9c740828b27c900d8f17
PeterRanario <peterranario11@gmail.com>
2024-12-03 23:48:31 +0800

    Added method to push a stack

--- It20c-Ranario/src/topic_3_stack_calculator/Stack.java
+++ It20c-Ranario/src/topic_3_stack_calculator/Stack.java
@@ -32,5 +32,13 @@ public class Stack {
     public int size() {
         return top + 1;
     }
+    
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push " + value);
+            return;
+        }
+        stackArray[++top] = value;
+    }
 
 }
