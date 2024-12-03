78dd9ca42e3b01deea34d83911c9b45d11f048e7
PeterRanario <peterranario11@gmail.com>
2024-12-04 01:03:13 +0800

    Initial commit on insertion sort

---
+++ It20c-Ranario/src/topic_6_sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic_6_sorting;
+
+/**
+ *
+ * @author Peter
+ */
+public class InsertionSort {
+
+    public static void insertionSort(int[] data) {
+        int size = data.length;
+        for (int index = 1; index < size; index++) {
+            int currentElement = data[index];
+            int position = index - 1;
+        }
+    }
+
+}
