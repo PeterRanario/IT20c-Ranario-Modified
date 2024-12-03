b66078ee92a5d0558454973c247957aa0ca80e30
PeterRanario <peterranario11@gmail.com>
2024-12-03 21:12:00 +0800

    Initial commit on main class.

--- It20c-Ranario/src/topic_3_stack/Main.java
+++ It20c-Ranario/src/topic_3_stack/Main.java
@@ -5,10 +5,22 @@
  */
 package topic_3_stack;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Peter
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Stack stack = new Stack(5);
+
+        System.out.println("Good day! Hello.");
+
+    }
+
 }
