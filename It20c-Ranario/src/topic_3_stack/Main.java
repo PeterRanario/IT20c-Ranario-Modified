f298347cfe2d8f1f699c0072be37d17b9725491b
PeterRanario <peterranario11@gmail.com>
2024-12-03 21:26:18 +0800

    Added User input on main class.

--- It20c-Ranario/src/topic_3_stack/Main.java
+++ It20c-Ranario/src/topic_3_stack/Main.java
@@ -21,6 +21,38 @@ public class Main {
 
         System.out.println("Good day! Hello.");
 
+        while (true) {
+            System.out.println("Please choose what you want to do: "
+                    + " 1. Size"
+                    + " 2. Push"
+                    + " 3. Pop"
+                    + " 4. Exit");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Size of stack is: " + stack.size());
+                    System.out.println("---------------");
+                    break;
+                case 2:
+                    System.out.println("Add a number: ");
+                    int num = sc.nextInt();
+                    System.out.println("Value has been pushed: " + num);
+                    stack.push(num);
+                    stack.size();
+                    System.out.println("--------------");
+                    break;
+                case 3:
+                    stack.pop();
+                    System.out.println("Element popped: " + stack.pop());
+                    stack.size();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using this. Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid input!, Please choose a valid choice.");
+            }
+        }
     }
-
 }
