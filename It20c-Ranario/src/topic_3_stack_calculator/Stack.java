2108940662d246e30693bb6d559ca0920b90b19c
PeterRanario <peterranario11@gmail.com>
2024-12-03 23:49:18 +0800

    Added method to peek and pop a stack

--- It20c-Ranario/src/topic_3_stack_calculator/Stack.java
+++ It20c-Ranario/src/topic_3_stack_calculator/Stack.java
@@ -40,5 +40,21 @@ public class Stack {
         }
         stackArray[++top] = value;
     }
+    
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek.");
+            return -1; 
+        }
+        return stackArray[top];
+    }
+    
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop.");
+            return -1;
+        }
+        return stackArray[top--];
+    }
 
 }
