e0cfe92295f7f847c9d1f9bb69b8a2f705622535
PeterRanario <peterranario11@gmail.com>
2024-12-04 01:14:31 +0800

    Added array length

--- It20c-Ranario/src/topic_6_sorting/MergeSort.java
+++ It20c-Ranario/src/topic_6_sorting/MergeSort.java
@@ -15,6 +15,17 @@ public class MergeSort {
         if (array.length < 2) {
             return;
         }
+        int middle = array.length / 2;
+        int[] leftHalf = new int[middle];
+        int[] rightHalf = new int[array.length - middle];
+
+        System.arraycopy(array, 0, leftHalf, 0, middle);
+        System.arraycopy(array, middle, rightHalf, 0, array.length - middle);
+
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+
+        merge(array, leftHalf, rightHalf);
     }
 
 }
